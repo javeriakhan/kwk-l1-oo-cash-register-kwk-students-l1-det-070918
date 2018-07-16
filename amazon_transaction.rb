71dcf5706a9d25424f9e087674c263af26f70992
javeriakhan <javeriak1017@gmail.com>
2018-07-16 15:48:10 +0000

    automatically backed up by learn

--- amazon_transaction.rb
+++ amazon_transaction.rb
@@ -1,1 +1,11 @@
-# Code your cash register here!
+class AmazonTransaction
+amazontransaction = AmazonTransaction.new
+def initialize(total, items, discount)
+  @total = 0
+  @items = []
+  @discount = 20 
+  end 
+end 
+
+def total 
+  AmazonTransaction.total = 100 
